591e4f1e4f7fc46d6309f2ee26f787fe80f44841
Chyr0s <chris@mouzourides.com>
2021-01-10 15:08:48 +0000

    Implemented colour of embedded character

--- ArctosPy.py
+++ ArctosPy.py
@@ -38,13 +38,26 @@ async def create_pc(context,arguments=None):
     if arguments == "-quick" or arguments == "-q":
         characterAPI.random()
     character_details = characterAPI.character_dict
-    character_embed=discord.Embed(title=character_details["Name"], description=(character_details["Race"] + " " + character_details["Class"]))
+    colour_dict = {"artificer":12040119,
+    "barbarian":5912868,
+    "bard":4176082,
+    "cleric":15462847,
+    "druid":3315769,
+    "fighter":7745327,
+    "monk":5464725,
+    "paladin":16777215,
+    "ranger":4994608,
+    "rogue":4526353,
+    "sorcerer":5451621,
+    "warlock":7738454,
+    "wizard":8084443}
+    character_embed=discord.Embed(title=character_details["Name"], description=(character_details["Race"] + " " + character_details["Class"]), colour=discord.Colour(colour_dict[character_details["Class"]]))
     character_embed.add_field(name="Strength", value=character_details["Stats"]["strength"], inline=True)
-    character_embed.add_field(name="Charisma", value=character_details["Stats"]["charisma"], inline=True)
-    character_embed.add_field(name="Constitution", value=character_details["Stats"]["constitution"], inline=True)
-    character_embed.add_field(name="Wisdom", value=character_details["Stats"]["wisdom"], inline=True)
-    character_embed.add_field(name="Intelligence", value=character_details["Stats"]["intelligence"], inline=True)
     character_embed.add_field(name="Dexterity", value=character_details["Stats"]["dexterity"], inline=True)
+    character_embed.add_field(name="Constitution", value=character_details["Stats"]["constitution"], inline=True)
+    character_embed.add_field(name="Intelligence", value=character_details["Stats"]["intelligence"], inline=True)
+    character_embed.add_field(name="Wisdom", value=character_details["Stats"]["wisdom"], inline=True)
+    character_embed.add_field(name="Charisma", value=character_details["Stats"]["charisma"], inline=True)
     await context.send(embed=character_embed)
 
 bot.run(disc_token)
